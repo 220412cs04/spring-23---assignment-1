e7e5d79c0334c68fdf67330d3e27283f7d1b786a
Angela Choi <113422424+220412cs04@users.noreply.github.com>
2023-02-22 15:27:06 +0500

    Add files via upload

---
+++ Pair.cpp
@@ -0,0 +1,9 @@
+//
+//  Pair.cpp
+//  spring'23 - assignment #1
+//
+//  Created by Angela Choi on 22/02/23.
+//
+
+#include "Pair.hpp"
+
---
+++ Pair.hpp
@@ -0,0 +1,20 @@
+//
+//  Pair.hpp
+//  spring'23 - assignment #1
+//
+//  Created by Angela Choi on 22/02/23.
+//
+
+#ifndef Pair_hpp
+#define Pair_hpp
+
+#include <iostream>
+using namespace std;
+
+struct Pair{
+    //members
+    string ownerName;
+    string dogName;
+};
+
+#endif /* Pair_hpp */
